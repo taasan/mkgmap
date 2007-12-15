05cd32b7e341ad46c27a458b3ff07de2a0180b8b
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2007-12-15 14:07:00 +0000

    Print out the header and the fields we think are pointers and sizes.  This should lead to finding the other pointers.

--- branches/typfile/test/java/test/TypTest.java
+++ branches/typfile/test/java/test/TypTest.java
@@ -16,22 +16,25 @@
  */
 package test;
 
-import uk.me.parabola.imgfmt.sys.FileImgChannel;
 import uk.me.parabola.imgfmt.app.CommonHeader;
+import static uk.me.parabola.imgfmt.app.CommonHeader.COMMON_HEADER_LEN;
+import uk.me.parabola.imgfmt.app.ReadStrategy;
 import uk.me.parabola.imgfmt.app.TYPFile;
 import uk.me.parabola.imgfmt.app.TYPHeader;
-import uk.me.parabola.imgfmt.app.ReadStrategy;
 import uk.me.parabola.imgfmt.fs.ImgChannel;
+import uk.me.parabola.imgfmt.sys.FileImgChannel;
 
 import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.List;
 
 /**
  * @author Steve Ratcliffe
  */
 public class TypTest {
-	private static final int FLEN = 10;
 
 	public static void main(String[] args) throws FileNotFoundException {
 		String name = args[0];
@@ -65,8 +68,8 @@ public class TypTest {
 
 	private static void printHeader(CommonHeader header) {
 		System.out.println("Common Header");
-		System.out.printf("%-" + FLEN + "s: %s\n", "Type", header.getType());
-		System.out.printf("%-10s: %d\n", "Date", header.getHeaderLength());
+		System.out.printf("%-10s: %s\n", "Type", header.getType());
+		System.out.printf("%-10s: %d\n", "Length", header.getHeaderLength());
 		System.out.printf("%-10s: %s\n", "Date", header.getCreationDate());
 
 		printHeader((TYPHeader) header);
@@ -79,9 +82,65 @@ public class TypTest {
 	private static void printHeader(TYPHeader header) {
 		byte[] un = header.getUnknown();
 
-		int off = TYPHeader.COMMON_HEADER_LEN;
+		int off = COMMON_HEADER_LEN;
 
-		for (int i = 0; i < un.length; i++) {
+		System.out.println("\nFile header");
+
+		off = printUnknown(off, un, 0x1f);
+		int value = getInt(un, 0x1f);
+		int size = getInt(un, 0x23);
+		//off = printInt(off, "Line def offset", value);
+		//off = printInt(off, "Line def size", size);
+		Section section = Section.addSection("Lines", value, size);
+		off = printSection(off, "sect1", section);
+
+		value = getInt(un, 0x27);
+		size = getInt(un, 0x2b);
+		//off = printInt(off, "u sect1", value);
+		//off = printInt(off, "u sect1 size?", size);
+		section = Section.addSection("u sect1", value, size);
+		off = printSection(off, "sect2", section);
+
+		char cvalue = getShort(un, 0x2f);
+		off = printUShort(off, "product", cvalue);
+		
+		off = printUnknown(off, un, 0x3d);
+
+		value = getInt(un, 0x3d);
+		off = printInt(off, "u sect3", value);
+
+		off = printUnknown(off, un, 0x51);
+
+		value = getInt(un, 0x51);
+		off = printInt(off, "polygon stack", value);
+
+		off = printUnknown(off, un, 0x5b);
+
+
+		System.out.println("end offset is " + Integer.toHexString(off));
+		System.exit(0);
+	}
+
+	/**
+	 * print a section.  Allows us to print the end offset which may help in
+	 * finding other sections.
+	 * @param off Offset to print.
+	 * @param desc The description.
+	 * @param sect The section to print.
+	 * @return The new offset.
+	 */
+	private static int printSection(int off, String desc, Section sect) {
+		printOffDesc(off, desc);
+		System.out.format("Off: %8x,    Next: %8x\n", sect.getOffset(),
+				sect.getOffset()+sect.getLen());
+
+		return off + 8;
+	}
+
+	private static int printUnknown(int startoff, byte[] un, int end) {
+		int commLen = COMMON_HEADER_LEN;
+		int off = startoff;
+		for (int i = startoff-commLen; i < end-commLen; i++) {
 			int b = un[i] & 0xff;
 			int c = b;
 			if (i < un.length - 2)
@@ -92,9 +151,77 @@ public class TypTest {
 			int i4 = i3;
 			if (i < un.length - 4)
 				i4 = i3 | ((un[i+3] & 0xff) << 24);
-			System.out.printf("%#06x  %-10s: %#04x (%3d) %#06x (%5d) %#08x (%8d) %#010x (%10d)\n", 
-					off++, "Unknown",
+			printOffDesc(off++, "???");
+			System.out.printf(
+					"%#04x (%3d) %#06x (%5d) %#08x (%8d) %#010x (%10d)\n",
 					b, b, c, c, i3, i3, i4, i4);
+		}
+		return off;
+	}
+
+	private static int printInt(int offset, String desc, int value) {
+		printOffDesc(offset, desc);
+		System.out.printf("%08x (%d)\n", value, value);
+		return offset + 4;
+	}
+
+	private static int printUShort(int offset, String desc, char cvalue) {
+		printOffDesc(offset, desc);
+		System.out.printf("%04x (%d)\n", (int) cvalue, (int) cvalue);
+		return offset + 2;
+	}
+
+	private static void printOffDesc(int offset, String desc) {
+		System.out.printf("%#06x:  %-20s: ", offset, desc);
+	}
+
+	private static int getInt(byte[] un, int off) {
+		int ind = off - COMMON_HEADER_LEN;
+		return (un[ind] & 0xff)
+				+ (((un[ind+1]) & 0xff) << 8)
+				+ (((un[ind+2]) & 0xff) << 16)
+				+ (((un[ind+3]) & 0xff) << 24)
+				;
+	}
+
+	private static char getShort(byte[] un, int off) {
+		int ind = off - COMMON_HEADER_LEN;
+		return (char) ((un[ind] & 0xff)
+				+ (((un[ind+1]) & 0xff) << 8)
+		);
+	}
+
+	static class Section {
+
+		private String description;
+		private int offset;
+		private int len;
+		private static List<Section> list = new ArrayList<Section>();
+
+		public static Section addSection(String desc, int off, int size) {
+			Section section = new Section();
+			section.description = desc;
+			section.offset = off;
+			section.len = size;
+
+			list.add(section);
+			return section;
+		}
+
+		public static List<Section> getList() {
+			return list;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public int getOffset() {
+			return offset;
+		}
+
+		public int getLen() {
+			return len;
 		}
 	}
 }
