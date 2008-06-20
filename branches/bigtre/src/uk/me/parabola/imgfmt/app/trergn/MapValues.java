62d455498fa872abca57129d314a65fb2de5d9d3
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2008-06-20 22:32:11 +0000

    Add some javadoc and rename methods etc.

--- branches/bigtre/src/uk/me/parabola/imgfmt/app/trergn/MapValues.java
+++ branches/bigtre/src/uk/me/parabola/imgfmt/app/trergn/MapValues.java
@@ -25,6 +25,7 @@ package uk.me.parabola.imgfmt.app.trergn;
  * written there.
  *
  * @author Steve Ratcliffe
+ * @see <a href="http://svn.parabola.me.uk/display/trunk/src/test/display/TreCalc.java">TreCalc.java</a>
  */
 public class MapValues {
 	private int mapId;
@@ -32,13 +33,16 @@ public class MapValues {
 
 	private byte[][] values = new byte[4][8];
 
+	// Converts the digits in the map id to the values seen in this section.
 	private static byte[] mapIdCodeTable = new byte[] {
 			0, 1, 0xf, 5,
 			0xd, 4, 7, 6,
 			0xb, 9, 0xe, 8,
 			2, 0xa, 0xc, 3
 	};
-	
+
+	// Used to work out the required offset that is applied to all the
+	// digits of the values.
 	private int[] offsetMap = new int[] {
 			6, 7, 5, 11,
 			3, 10, 13, 12,
@@ -51,6 +55,10 @@ public class MapValues {
 		this.length = headerLength;
 	}
 
+	/**
+	 * There are four values.  Get value n.
+	 * @param n Get value n, starting at 0 up to four.
+	 */
 	public int value(int n) {
 		byte[] out = values[n];
 
@@ -62,11 +70,12 @@ public class MapValues {
 	}
 
 	public void calculate() {
-		// Done in this order so we can reuse values that are calculated earlier.
-		thirdValue();
-		fourthValue();
-		firstValue();
-		secondValue();
+		// Done in this order because the first and second depend on things
+		// we have already calculated in three.
+		calcThird();
+		calcFourth();
+		calcFirst();
+		calcSecond();
 
 		addOffset();
 	}
@@ -77,7 +86,7 @@ public class MapValues {
 	private void addOffset() {
 		// To get the offset value we add up all the even nibbles of the map
 		// number and transform via a table.
-		int n = mapidNibble(1) + mapidNibble(3) + mapidNibble(5) + mapidNibble(7);
+		int n = mapIdDigit(1) + mapIdDigit(3) + mapIdDigit(5) + mapIdDigit(7);
 		System.out.println("raw is " + n);
 
 		int offset = offsetMap[n & 0xf];
@@ -88,34 +97,43 @@ public class MapValues {
 		}
 	}
 
-	private void firstValue() {
+	/**
+	 * This value is made from the third value, combined with the raw
+	 * map id values.
+	 */
+	private void calcFirst() {
 		byte[] out = values[0];
 		byte[] v3 = values[3];
 
 		// First bytes are the low bytes of the mapId, with the corresponding
 		// value from value[3] added.
-		out[0] = (byte) (mapidNibble(4) + v3[0]);
-		out[1] = (byte) (mapidNibble(5) + v3[1]);
-		out[2] = (byte) (mapidNibble(6) + v3[2]);
-		out[3] = (byte) (mapidNibble(7) + v3[3]);
+		out[0] = (byte) (mapIdDigit(4) + v3[0]);
+		out[1] = (byte) (mapIdDigit(5) + v3[1]);
+		out[2] = (byte) (mapIdDigit(6) + v3[2]);
+		out[3] = (byte) (mapIdDigit(7) + v3[3]);
 
 		// Copies of v3
 		out[4] = v3[4];
 		out[5] = v3[5];
 		out[6] = v3[6];
 
-		// Always (?) one more
+		// Always (?) one more.  The one likely comes from some other
+		// part of the header, but we don't know if or where.
 		out[7] = (byte) (v3[7] + 1);
 	}
 
-	private void secondValue() {
+	/**
+	 * This is made from various parts of the third value and the raw digits
+	 * from the map id.  There are two digits where the header length digits
+	 * are used (or that could be a coincidence, but it holds up well so far).
+	 */
+	private void calcSecond() {
 		byte[] out = values[1];
 		byte[] v3 = values[3];
 
 		// Just same as in v3
 		out[0] = v3[0];
 		out[1] = v3[1];
-
 
 		int h1 = length >> 4;
 		int h2 = length;
@@ -124,20 +142,20 @@ public class MapValues {
 
 		// The following are the sum of individual nibbles in U3 and the
 		// corresponding nibble in the top half of mapId.
-		out[4] = (byte) (v3[4] + mapidNibble(0));
-		out[5] = (byte) (v3[5] + mapidNibble(1));
-		out[6] = (byte) (v3[6] + mapidNibble(2));
-		out[7] = (byte) (v3[7] + mapidNibble(3));
+		out[4] = (byte) (v3[4] + mapIdDigit(0));
+		out[5] = (byte) (v3[5] + mapIdDigit(1));
+		out[6] = (byte) (v3[6] + mapIdDigit(2));
+		out[7] = (byte) (v3[7] + mapIdDigit(3));
 	}
 
 	/**
-	 * This is just the hex digits of the map id in a given order
+	 * This is made of the hex digits of the map id in a given order
 	 * translated according to a given table of values.
 	 */
-	private void thirdValue() {
+	private void calcThird() {
 		byte[] out = values[2];
 		for (int i = 0; i < 8; i++) {
-			int n = mapidNibble(i);
+			int n = mapIdDigit(i);
 			out[(i ^ 1)] = mapIdCodeTable[n];
 		}
 	}
@@ -145,7 +163,7 @@ public class MapValues {
 	/**
 	 * This is just a copy of the third value.
 	 */
-	private void fourthValue() {
+	private void calcFourth() {
 		System.arraycopy(values[2], 0, values[3], 0, values[3].length);
 	}
 
@@ -154,7 +172,7 @@ public class MapValues {
 	 * @param i The nibble number, 0 most significant, 7 the least.
 	 * @return The given nibble of the map id.
 	 */
-	private int mapidNibble(int i) {
+	private int mapIdDigit(int i) {
 		return (mapId >>> (4 * (7 - i))) & 0xf;
 	}
 }
