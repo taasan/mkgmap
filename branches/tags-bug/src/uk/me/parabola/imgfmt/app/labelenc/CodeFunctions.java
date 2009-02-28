9cd4588ae6495c4d4494d2722845a2f32f4e7079
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2009-02-28 12:41:35 +0000

    branch for tags bugs

---
+++ branches/tags-bug/src/uk/me/parabola/imgfmt/app/labelenc/CodeFunctions.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2007 Steve Ratcliffe
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * 
+ * Author: Steve Ratcliffe
+ * Create date: Jan 1, 2008
+ */
+package uk.me.parabola.imgfmt.app.labelenc;
+
+/**
+ * @author Steve Ratcliffe
+ */
+public class CodeFunctions {
+	// Label encoding length
+	public static final int ENCODING_FORMAT6 = 6;
+	private static final int ENCODING_FORMAT9 = 9;
+	private static final int ENCODING_FORMAT10 = 10;
+
+	private int encodingType;
+	private CharacterEncoder encoder;
+	private CharacterDecoder decoder;
+
+	protected void setEncoder(CharacterEncoder encoder) {
+		this.encoder = encoder;
+	}
+
+	public CharacterEncoder getEncoder() {
+		return encoder;
+	}
+
+	protected void setDecoder(CharacterDecoder decoder) {
+		this.decoder = decoder;
+	}
+
+	public CharacterDecoder getDecoder() {
+		return decoder;
+	}
+
+	public int getEncodingType() {
+		return encodingType;
+	}
+
+	protected void setEncodingType(int encodingType) {
+		this.encodingType = encodingType;
+	}
+
+	/**
+	 * Create a CharacterEncoder for the given charset option.  Note that this
+	 * routine also writes to the lblHeader parameter to set the encoding type.
+	 * @param charset The mkgmap command line option to be interpreted.
+	 * @return The various character set parameters that will be needed.
+	 */
+	public static CodeFunctions createEncoderForLBL(String charset) {
+		CodeFunctions funcs = new CodeFunctions();
+
+		if ("ascii".equals(charset)) {
+			funcs.setEncodingType(ENCODING_FORMAT6);
+			funcs.setEncoder(new Format6Encoder());
+			funcs.setDecoder(new Format6Decoder());
+		} else if ("latin1".equals(charset)) {
+			funcs.setEncodingType(ENCODING_FORMAT9);
+			funcs.setEncoder(new Latin1Encoder());
+		} else if ("latin2".equals(charset)) {
+			funcs.setEncodingType(ENCODING_FORMAT6);
+			funcs.setEncoder(new Format6Encoder());
+		} else if ("unicode".equals(charset)) {
+			funcs.setEncodingType(ENCODING_FORMAT10);
+			funcs.setEncoder(new Utf8Encoder());
+			funcs.setDecoder(new Utf8Decoder());
+		} else if ("simple8".equals(charset)) {
+			funcs.setEncodingType(ENCODING_FORMAT9);
+			funcs.setEncoder(new Simple8Encoder());
+		} else {
+			funcs.setEncodingType(ENCODING_FORMAT9);
+			funcs.setEncoder(new AnyCharsetEncoder(charset));
+		}
+
+		return funcs;
+	}
+
+	/**
+	 * Sets encoding functions for a given format and code page.  This is used
+	 * when reading from an existing file.
+	 *
+	 * @param format The format from the lbl header.
+	 * @return The various character set parameters that will be needed.
+	 */
+	public static CodeFunctions createEncoderForLBL(int format) {
+		CodeFunctions funcs = new CodeFunctions();
+
+		if (format == ENCODING_FORMAT6) {
+			funcs.setEncodingType(ENCODING_FORMAT6);
+			funcs.setEncoder(new Format6Encoder());
+			funcs.setDecoder(new Format6Decoder());
+		} else {
+			// TODO TEMP...
+			funcs.setEncodingType(ENCODING_FORMAT9);
+			funcs.setEncoder(new Latin1Encoder());
+			funcs.setDecoder(new SimpleDecoder());
+		}
+
+		return funcs;
+	}
+
+	public static CharacterEncoder getDefaultEncoder() {
+		return new Format6Encoder();
+	}
+
+	public static CharacterDecoder getDefaultDecoder() {
+		return new Format6Decoder();
+	}
+}
---
+++ branches/tags-bug/src/uk/me/parabola/imgfmt/app/lbl/City.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2007 Steve Ratcliffe
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * 
+ * Author: Steve Ratcliffe
+ * Create date: Jan 1, 2008
+ */
+package uk.me.parabola.imgfmt.app.lbl;
+
+import uk.me.parabola.imgfmt.app.ImgFileWriter;
+import uk.me.parabola.imgfmt.app.Label;
+import uk.me.parabola.imgfmt.app.trergn.Subdivision;
+
+/**
+ * A city is in a region.  It also has (or can have anyway) a reference to
+ * an indexed point within the map itself.
+ * 
+ * @author Steve Ratcliffe
+ */
+public class City {
+	private static final int POINT_REF = 0x8000;
+	private static final int REGION_IS_COUNTRY = 0x4000;
+
+	private final int index;
+
+	private final Region region;
+	private final Country country;
+
+	// This determines if a label is being used or a subdivision and point
+	// combo.
+	private boolean pointRef;
+
+	// The location of the city.  These could both be zero if we are using a
+	// label instead.
+	private Subdivision subdivision;
+	private byte pointIndex;
+
+
+	// You can have either a label or a subdivision and point.  This will be
+	// null if the location is being specified.
+	private Label label;
+
+	public City(Region region, int index) {
+		this.region = region;
+		this.country = null;
+		this.index = index;
+	}
+
+	public City(Country country, int index) {
+		this.country = country;
+		this.region = null;
+		this.index = index;
+	}
+
+	void write(ImgFileWriter writer) {
+		//writer.put3()
+		if (pointRef) {
+		    //		    System.err.println("City point = " + (int)pointIndex + " div = " + subdivision.getNumber());
+			writer.put(pointIndex);
+			writer.putChar((char)subdivision.getNumber());
+		} else {
+			writer.put3(label.getOffset());
+		}
+
+		char info;
+		if(region != null)
+		    info = (char) (region.getIndex() & 0x3fff);
+		else
+		    info = (char) (REGION_IS_COUNTRY | (country.getIndex() & 0x3fff));
+		if (pointRef)
+			info |= POINT_REF;
+
+		writer.putChar(info);
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setLabel(Label label) {
+		pointRef = false;
+		this.label = label;
+	}
+
+	public void setPointIndex(byte pointIndex) {
+		pointRef = true;
+		this.pointIndex = pointIndex;
+	}
+
+	public void setSubdivision(Subdivision subdivision) {
+		pointRef = true;
+		this.subdivision = subdivision;
+	}
+}
