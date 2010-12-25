7408d046c1afc587e9252de6dbdbc6f87118144b
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2010-12-25 16:24:13 +0000

    Create branch for index sorting mods.

---
+++ branches/index/src/uk/me/parabola/imgfmt/mps/MapsetBlock.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2007 Steve Ratcliffe
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * 
+ * Author: Steve Ratcliffe
+ * Create date: Dec 19, 2007
+ */
+package uk.me.parabola.imgfmt.mps;
+
+import uk.me.parabola.io.StructuredOutputStream;
+
+import java.io.IOException;
+
+/**
+ * Block describing the map set.
+ *
+ * @author Steve Ratcliffe
+ */
+public class MapsetBlock extends Block {
+	private static final int BLOCK_TYPE = 0x56;
+	
+	private String name = "OSM map set";
+	
+	public MapsetBlock() {
+		super(BLOCK_TYPE);
+	}
+
+	protected void writeBody(StructuredOutputStream out) throws IOException {
+		out.writeString(name);
+		out.write(0); // unknown
+	}
+
+	public void setName(String name) {
+		if (name != null)
+			this.name = name;
+	}
+}
