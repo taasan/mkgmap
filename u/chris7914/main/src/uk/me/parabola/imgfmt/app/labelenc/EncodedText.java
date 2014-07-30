1e37f98c5b92dcf450969446aafb8aeaddd17764
svn <svn@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2014-07-30 12:04:00 +0000

    Create branch main for chris7914

---
+++ u/chris7914/main/src/uk/me/parabola/imgfmt/app/labelenc/EncodedText.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2007,2014 Steve Ratcliffe
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * 
+ * Author: Steve Ratcliffe
+ * Create date: 14-Jan-2007
+ */
+package uk.me.parabola.imgfmt.app.labelenc;
+
+/**
+ * Holds the bytes and length of an encoded character string used in a label.
+ * The length of the byte array may be longer than the part that is actually
+ * used, so the length property should always be used.
+ *
+ * Class is immutable.
+ *
+ * @author Steve Ratcliffe
+ */
+public class EncodedText {
+	private final int hashCode;
+	private final byte[] ctext;
+	private final int length;
+	private final char[] chars;
+
+	public EncodedText(byte[] buf, int len, char[] chars) {
+		this.ctext = buf;
+		this.length = len;
+		this.chars = chars;
+
+		int hc = 0;
+		for (int i = 0; i < length; i++)
+			hc = 31*hc + ctext[i];
+		hashCode = hc;
+	}
+
+	public byte[] getCtext() {
+		return ctext;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public char[] getChars() {
+		return chars;
+	}
+
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		EncodedText that = (EncodedText) o;
+
+		if (length != that.length) return false;
+		for (int i = 0; i < length; i++)
+			if (ctext[i] != that.ctext[i])
+				return false;
+
+		return true;
+	}
+
+	public int hashCode() {
+		return hashCode;
+	}
+}
---
+++ u/chris7914/main/src/uk/me/parabola/mkgmap/osmstyle/actions/DeleteAction.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2008 Steve Ratcliffe
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * 
+ * Author: Steve Ratcliffe
+ * Create date: 29-Nov-2008
+ */
+package uk.me.parabola.mkgmap.osmstyle.actions;
+
+import uk.me.parabola.mkgmap.reader.osm.Element;
+import uk.me.parabola.mkgmap.reader.osm.TagDict;
+
+/**
+ * Deletes a tag.
+ * 
+ * @author Steve Ratcliffe
+ */
+public class DeleteAction implements Action {
+	private final short tag;
+
+	public DeleteAction(String tag) {
+		this.tag = TagDict.getInstance().xlate(tag);
+	}
+
+	public boolean perform(Element el) {
+		String oldVal = el.deleteTag(tag);
+		return oldVal != null;
+	}
+
+	public String toString() {
+		return "delete " + TagDict.getInstance().get(tag) + ";";
+	}
+}
