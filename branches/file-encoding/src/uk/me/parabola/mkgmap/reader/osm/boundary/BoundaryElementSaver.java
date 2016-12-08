f3d9cc487a0d01c085c92a9118db452a3ce157e8
steve <steve@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2016-12-08 12:45:52 +0000

    Create branch for file encoding issues.

---
+++ branches/file-encoding/src/uk/me/parabola/mkgmap/reader/osm/boundary/BoundaryElementSaver.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright (C) 2006, 2011.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 or
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+package uk.me.parabola.mkgmap.reader.osm.boundary;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map.Entry;
+
+import uk.me.parabola.log.Logger;
+import uk.me.parabola.mkgmap.build.Locator;
+import uk.me.parabola.mkgmap.reader.osm.Element;
+import uk.me.parabola.mkgmap.reader.osm.ElementSaver;
+import uk.me.parabola.mkgmap.reader.osm.Node;
+import uk.me.parabola.mkgmap.reader.osm.OsmConverter;
+import uk.me.parabola.mkgmap.reader.osm.Relation;
+import uk.me.parabola.mkgmap.reader.osm.Tags;
+import uk.me.parabola.mkgmap.reader.osm.Way;
+import uk.me.parabola.util.EnhancedProperties;
+
+/**
+ * This saver only keeps ways with <code>natural=coastline</code> tags. This is
+ * used for loading of extra coastline files.
+ * 
+ * @author WanMil
+ */
+public class BoundaryElementSaver extends ElementSaver {
+	private static final Logger log = Logger.getLogger(BoundaryElementSaver.class);
+
+	private final static Locator locator = new Locator();
+	
+	private final BoundarySaver saver;
+	
+	public BoundaryElementSaver(EnhancedProperties args, BoundarySaver saver) {
+		super(args);
+		this.saver = saver;
+	}
+
+	/**
+	 * Checks if the given element is an administrative boundary or a
+	 * postal code area.
+	 * @param element an element
+	 * @return <code>true</code> administrative boundary or postal code; 
+	 * <code>false</code> element cannot be used for precompiled bounds 
+	 */
+	public static boolean isBoundary(Element element) {
+		if (element instanceof Relation) {
+			String type = element.getTag("type");
+			
+			if ("boundary".equals(type) || "multipolygon".equals(type)) {
+				String boundaryVal = element.getTag("boundary");
+				if ("administrative".equals(boundaryVal)) {
+					// for boundary=administrative the admin_level must be set
+					if (element.getTag("admin_level") == null) {
+						return false;
+					}
+					
+					// Check for admin_level==2 if the name is known in the LocatorConfig.xml.
+					// This should abandon all non country admin_level 2 boundaries
+					if (element.getTag("admin_level").equals("2")) {
+						Tags copyTags = new Tags();
+						for (Entry<String,String> tag : element.getTagEntryIterator()) {
+							copyTags.put(tag.getKey(), tag.getValue());
+						}
+						String iso = locator.getCountryISOCode(copyTags);
+						if (iso == null) {
+							log.warn("Ignore admin_level 2 element:", element.toBrowseURL(), element.toTagString());
+							return false;
+						}
+					}
+					// and a name must be set (check only for a tag containing name
+					for (Entry<String,String> tag : element.getTagEntryIterator()) {
+						if (tag.getKey().contains("name")) {
+							return true;
+						}
+					}
+					// does not contain a name tag => do not use it
+					return false;					
+				} else if ("postal_code".equals(boundaryVal)) {
+					// perform a positive check
+					
+					// is postal_code set?
+					if (element.getTag("postal_code") != null) {
+						return true;
+					}
+					// and a name must be set (check only for a tag containing name
+					for (Entry<String,String> tag : element.getTagEntryIterator()) {
+						if (tag.getKey().contains("name")) {
+							return true;
+						}
+					}
+					// does not contain a name tag => do not use it
+					return false;						
+				} else if (element.getTag("postal_code") != null){
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} else if (element instanceof Way) {
+			Way w = (Way) element;
+			// a single way must be closed
+			if (w.isClosedInOSM() == false) {
+				return false;
+			}
+			// the boundary tag must be "administrative" or "postal_code"
+			String boundaryVal = element.getTag("boundary");
+			if ("administrative".equals(boundaryVal)) {
+				// for boundary=administrative the admin_level must be set
+				if (element.getTag("admin_level") == null) {
+					return false;
+				}
+
+				// Check for admin_level==2 if the name is known in the LocatorConfig.xml.
+				// This should abandon all non country admin_level 2 boundaries
+				if (element.getTag("admin_level").equals("2")) {
+					Tags copyTags = new Tags();
+					for (Entry<String,String> tag : element.getTagEntryIterator()) {
+						copyTags.put(tag.getKey(), tag.getValue());
+					}
+					String iso = locator.getCountryISOCode(copyTags);
+					if (iso == null) {
+						log.warn("Ignore admin_level 2 element:", element.toBrowseURL(), element.toTagString());
+						return false;
+					}
+				}
+				
+				// and a name must be set (check only for a tag containing name)
+				for (Entry<String,String> tag : element.getTagEntryIterator()) {
+					if (tag.getKey().contains("name")) {
+						return true;
+					}
+				}
+				// does not contain a name tag => do not use it
+				return false;
+			} else if ( "postal_code".equals(boundaryVal)) {
+				// the name tag must be set for it
+				return element.getTag("name") != null;
+			} else if (element.getTag("postal_code") != null) {
+				// postal_code as tag
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public void addRelation(Relation rel) {
+		if (isBoundary(rel)) {
+			BoundaryRelation bRel = (BoundaryRelation) createMultiPolyRelation(rel);
+			bRel.processElements();
+			Boundary b = bRel.getBoundary();
+			if (b != null)
+				saver.addBoundary(b);
+		} else {
+			log.warn("Relation is not processed due to missing tags:", rel.getId(), rel.toTagString());
+		}
+	}
+	
+	public void deferRelation(long id, Relation rel, String role) {
+		return;
+	}
+	
+	public Relation createMultiPolyRelation(Relation rel) {
+		return new BoundaryRelation(rel, wayMap, getBoundingBox());
+	}
+
+	public void addNode(Node node) {
+		return;
+	}
+
+	public void convert(OsmConverter converter) {
+		nodeMap = null;
+
+		converter.setBoundingBox(getBoundingBox());
+
+		ArrayList<Relation> relations = new ArrayList<Relation>(
+				relationMap.values());
+		relationMap = null;
+		Collections.reverse(relations);
+		for (int i = relations.size() - 1; i >= 0; i--) {
+			converter.convertRelation(relations.get(i));
+			relations.remove(i);
+		}
+
+
+		for (Way w : wayMap.values())
+			converter.convertWay(w);
+
+		wayMap = null;
+
+		converter.end();
+
+		relationMap = null;
+	}
+}
---
+++ branches/file-encoding/test/func/route/SimpleRouteTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2008 Steve Ratcliffe
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 or
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+/* Create date: 16-Feb-2009 */
+package func.route;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import uk.me.parabola.imgfmt.fs.DirectoryEntry;
+import uk.me.parabola.imgfmt.fs.FileSystem;
+import uk.me.parabola.mkgmap.main.Main;
+
+import func.Base;
+import func.lib.Args;
+import func.lib.RangeMatcher;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleRouteTest extends Base {
+
+	/**
+	 * Simple test to ensure that nothing has changed.  Of course
+	 * if the output should have changed, then this will have to be altered
+	 * to match.
+	 */
+	@Test
+	public void testSize() throws FileNotFoundException {
+		Main.mainNoSystemExit(new String[]{
+				Args.TEST_STYLE_ARG,
+				"--preserve-element-order",
+				"--route",
+				Args.TEST_RESOURCE_OSM + "uk-test-1.osm.gz",
+				Args.TEST_RESOURCE_MP + "test1.mp"
+		});
+
+		FileSystem fs = openFs(Args.DEF_MAP_ID + ".img");
+		assertNotNull("file exists", fs);
+
+		List<DirectoryEntry> entries = fs.list();
+		int count = 0;
+		for (DirectoryEntry ent : entries) {
+			String ext = ent.getExt();
+
+			int size = ent.getSize();
+			switch (ext) {
+			case "RGN":
+				count++;
+				System.out.println("RGN size " + size);
+				assertThat("RGN size", size, new RangeMatcher(127586));
+				break;
+			case "TRE":
+				count++;
+				System.out.println("TRE size " + size);
+				// Size varies depending on svn modified status
+				assertThat("TRE size", size, new RangeMatcher(1454, 2));
+				break;
+			case "LBL":
+				count++;
+				assertEquals("LBL size", 28742, size);
+				break;
+			case "NET":
+				count++;
+				assertEquals("NET size", 66859, size);
+				break;
+			case "NOD":
+				count++;
+				assertEquals("NOD size", 170201, size);
+				break;
+			}
+		}
+		assertTrue("enough checks run", count == 5);
+
+		fs = openFs(Args.DEF_MAP_FILENAME2);
+		assertNotNull("file exists", fs);
+
+		entries = fs.list();
+		count = 0;
+		for (DirectoryEntry ent : entries) {
+			String ext = ent.getExt();
+
+			int size = ent.getSize();
+			switch (ext) {
+			case "RGN":
+				count++;
+				System.out.println("RGN size " + size);
+				assertThat("RGN size", size, new RangeMatcher(2727));
+				break;
+			case "TRE":
+				count++;
+				System.out.println("TRE size " + size);
+				// Size varies depending on svn modified status
+				assertThat("TRE size", size, new RangeMatcher(769, 2));
+				break;
+			case "LBL":
+				count++;
+				assertEquals("LBL size", 999, size);
+				break;
+			case "NET":
+				count++;
+				assertEquals("NET size", 1301, size);
+				break;
+			case "NOD":
+				count++;
+				assertEquals("NOD size", 3584, size);
+				break;
+			}
+		}
+		assertTrue("enough checks run", count == 5);
+	}
+}
