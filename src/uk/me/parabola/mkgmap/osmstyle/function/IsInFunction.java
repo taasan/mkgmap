177c9713741f198fe8732c591f07c0f959c0280b
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2019-12-30 11:12:05 +0000

    fix compilation problems

--- src/uk/me/parabola/mkgmap/osmstyle/function/IsInFunction.java
+++ src/uk/me/parabola/mkgmap/osmstyle/function/IsInFunction.java
@@ -1,8 +1,8 @@
 package uk.me.parabola.mkgmap.osmstyle.function;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
-import java.util.List;
 
 import uk.me.parabola.mkgmap.reader.osm.Element;
 import uk.me.parabola.mkgmap.reader.osm.Node;
@@ -11,7 +11,6 @@ import uk.me.parabola.mkgmap.reader.osm.Way;
 public class IsInFunction extends StyleFunction {
 
 	private Map<Long, Way> wayMap;
-	private List<Way> matchTagPolygons;  // maybe this is some spatial structure instead
 
 	public IsInFunction() {
 		super(null);
@@ -24,19 +23,21 @@ public class IsInFunction extends StyleFunction {
 
 	protected String calcImpl(Element el) {
 		boolean answer = false;
-		if (matchTagPolygons == null) {
-		    matchTagPolygons = new ArrayList<Way>();
-		    // %%% loop over wayMap and, for closed ways,
-		    // if has tag param(0) with value param(1)
-		    // add to matchTagPolygons
-		}
-		// this is just some test code to give different answers...
-		if (el instanceof Node) {
-			// %%% loop over matchTagPolygons and determine if point is_in
-			answer = params.get(0).equals("polyTag");
-		} else if (el instanceof Way) {
-			// %%% similar, but for lines/areas
-			answer = params.get(2).equals("accuracy");
+		if (wayMap != null) {
+			/**
+			 * TODO: calculate bbox of element, 
+			 * retrieve closed ways which match the tag given with parameters
+			 * use spatial index if that improves performance
+			 * calculate insideness  
+			 */
+			
+			// this is just some test code to give different answers...
+			if (el instanceof Node) {
+				answer = params.get(0).equals("polyTag");
+			} else if (el instanceof Way) {
+				// %%% similar, but for lines/areas
+				answer = params.get(2).equals("accuracy");
+			}
 		}
 		return String.valueOf(answer);
 	}
@@ -67,8 +68,13 @@ public class IsInFunction extends StyleFunction {
 	}
 
 	@Override
+	public String toString() {
+		// TODO: check what is needed for class ExpressionArranger and RuleSet.compile()
+		return super.toString() + params;
+	}
+	
+	@Override
 	public void augmentWith(uk.me.parabola.mkgmap.reader.osm.ElementSaver elementSaver) {
-		// System.out.println("IsIn aug with " + elementSaver.getWays().size() + " ways");
 		wayMap = elementSaver.getWays();
 	}
 
