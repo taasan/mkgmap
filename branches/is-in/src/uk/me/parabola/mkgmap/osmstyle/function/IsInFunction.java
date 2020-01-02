bf01c07446a285c7d82bd65d8413a8f7b08bfba0
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2020-01-02 17:17:20 +0000

    check both lines and polygons with the same method As expected performance is poor when compared to ResidentialHook

--- branches/is-in/src/uk/me/parabola/mkgmap/osmstyle/function/IsInFunction.java
+++ branches/is-in/src/uk/me/parabola/mkgmap/osmstyle/function/IsInFunction.java
@@ -6,8 +6,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+import uk.me.parabola.imgfmt.Utils;
 import uk.me.parabola.imgfmt.app.Area;
 import uk.me.parabola.imgfmt.app.Coord;
+import uk.me.parabola.log.Logger;
 import uk.me.parabola.mkgmap.reader.osm.Element;
 import uk.me.parabola.mkgmap.reader.osm.Node;
 import uk.me.parabola.mkgmap.reader.osm.Way;
@@ -15,6 +17,7 @@ import uk.me.parabola.util.ElementQuadTree;
 import uk.me.parabola.util.Java2DConverter;
 
 public class IsInFunction extends StyleFunction {
+	private static final Logger log = Logger.getLogger(IsInFunction.class);
 
 	private ElementQuadTree qt;
 	
@@ -65,24 +68,51 @@ public class IsInFunction extends StyleFunction {
 							path.append(polyPath, false);
 						}
 						java.awt.geom.Area polygonsArea = new java.awt.geom.Area(path);
+						List<Coord> intersections = new ArrayList<>();
 						//TODO: need to know if this function is used in lines or polygons rules
-						if (w.hasIdenticalEndPoints()) {
-							java.awt.geom.Area a = Java2DConverter.createArea(w.getPoints());
-							java.awt.geom.Area inter = new java.awt.geom.Area(a);
-							inter.intersect(polygonsArea);
-							if (!inter.isEmpty()) {
-								if ("any".equals(params.get(2))) {
-									answer = true;
-								} else if ("all".equals(params.get(2))) {
-									answer = polygonsArea.getBounds2D().contains(a.getBounds2D()) && inter.equals(a);
+						List<List<Coord>> mergedShapes = Java2DConverter.areaToShapes(polygonsArea);
+						// brute force search for intersections between way segments
+						for (List<Coord> shape : mergedShapes) {
+							for (int i = 0; i < shape.size(); i++) {
+								Coord p11 = shape.get(i);
+								Coord p12 = shape.get(i + 1 < shape.size() ? i+1 : 0); 
+								for (int k = 0; k < w.getPoints().size() - 1; k++) {
+									Coord p21 = w.getPoints().get(k);
+									Coord p22 = w.getPoints().get(k + 1);
+									Coord x = Utils.getSegmentSegmentIntersection(p11, p12, p21, p22);
+									if (x != null) {
+										intersections.add(x);
+										// intersection found, maybe they are crossing, maybe they are touching 
+										double x1 = (double)p21.getHighPrecLon()/ (1<<Coord.DELTA_SHIFT); 
+										double y1 = (double)p21.getHighPrecLat() / (1<<Coord.DELTA_SHIFT); 
+										double x2 = (double)p22.getHighPrecLon()/ (1<<Coord.DELTA_SHIFT); 
+										double y2 = (double)p22.getHighPrecLat() / (1<<Coord.DELTA_SHIFT); 
+
+										// TODO: replace area.contains() 
+										// with method that returns only
+										// true for nodes which are inside
+										// and not on the boundary
+										if (polygonsArea.contains(x1,y1) != polygonsArea.contains(x2,y2)) {
+											if ("any".equals(params.get(2)))
+												return String.valueOf(true);
+											if ("all".equals(params.get(2)))
+												return String.valueOf(false);
+										}
+									}
 								}
 							}
-						} else {
-							// TODO: Check if POLYLINE is inside polygons 
 						}
+						// found no intersection, way is either inside or outside, use result for 1st point
+						// TODO: make sure that tested node is not on boundary
+						double x1 = (double) w.getFirstPoint().getHighPrecLon() / (1 << Coord.DELTA_SHIFT);
+						double y1 = (double) w.getFirstPoint().getHighPrecLat() / (1 << Coord.DELTA_SHIFT);
+						answer = polygonsArea.contains(x1, y1);
 					}
 				}
 			}
+		}
+		if (answer) {
+//			log.error(el.toBrowseURL(), params, answer);
 		}
 		return String.valueOf(answer);
 	}
