958f37f90b51044f7aa818151b65d553700a09de
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2020-02-07 13:17:38 +0000

    remove debug code and unused imports

--- src/uk/me/parabola/mkgmap/osmstyle/DirectoryFileLoader.java
+++ src/uk/me/parabola/mkgmap/osmstyle/DirectoryFileLoader.java
@@ -22,7 +22,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
