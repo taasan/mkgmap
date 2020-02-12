4b54e0f1f5278c98aa6c0134a6007ef56326a379
gerd <gerd@25d90789-57f7-4ee0-8453-03a3dfeeeb22>
2020-02-12 18:28:17 +0000

    is_in-function_v8b.patch by Ticker Berkin - Some restructuring with early stopping where possible. - Merging polygons for POINT IN/ON test so a point on shared boundary becomes IN rather than ON. - Not merging polygons when no need. - Make the function cacheable, so that there is negligible cost to the second call: highway=path & is_in(landuse, residential, all)=true [0xAA] highway=path & is_in(landuse, residential, all)=false [0xBB] - Improved the layout of documentation in the Style Manual. - Fixed quite a few problems.

--- test/uk/me/parabola/util/IsInUtilTest.java
+++ test/uk/me/parabola/util/IsInUtilTest.java
@@ -15,6 +15,8 @@ package uk.me.parabola.util;
 
 import static org.junit.Assert.assertTrue;
 
+import java.awt.geom.Path2D;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,6 +25,7 @@ import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
@@ -38,11 +41,105 @@ import uk.me.parabola.mkgmap.reader.osm.Node;
 import uk.me.parabola.mkgmap.reader.osm.OsmMapDataSource;
 import uk.me.parabola.mkgmap.reader.osm.Way;
 
+import uk.me.parabola.mkgmap.osmstyle.function.IsInFunction;
+
 public class IsInUtilTest {
 
+	private static int calcInsideness(FeatureKind kind, Element el, Set<Way> polygons) {
+		int result = 0;  
+		if (polygons == null || polygons.isEmpty()) { 
+			return IsInUtil.OUT;
+		}
+		
+		Area elementBbox;
+		if (el instanceof Node) {
+			Coord c = ((Node) el).getLocation();
+			for (Way polygon : polygons) {
+				switch (IsInUtil.isPointInShape(c, polygon.getPoints())) {
+				case IsInUtil.IN:
+					return IsInUtil.IN;
+				case IsInUtil.ON:
+					result |= IsInUtil.ON;
+				default:
+				}
+			}
+			return result == 0 ? IsInUtil.OUT : IsInUtil.ON;
+		} else if (el instanceof Way) {
+			Way w = (Way) el;
+			if (w.isComplete()) {
+				elementBbox = Area.getBBox(w.getPoints());
+				if (polygons.size() > 1) {
+					// combine all polygons which intersect the bbox of the element if possible
+					Path2D.Double path = new Path2D.Double();
+					for (Way polygon : polygons) {
+						path.append(Java2DConverter.createPath2D(polygon.getPoints()), false);
+					}
+					java.awt.geom.Area polygonsArea = new java.awt.geom.Area(path);
+					List<List<Coord>> mergedShapes = Java2DConverter.areaToShapes(polygonsArea);
+
+					// combination of polygons may contain holes. They are counter clockwise.
+					List<List<Coord>> holes = new ArrayList<>();
+					List<List<Coord>> outers = new ArrayList<>();
+					for (List<Coord> shape : mergedShapes) {
+						(Way.clockwise(shape) ? outers : holes).add(shape);
+					}
+					
+					// check if any outer intersects with the way
+					for (List<Coord> shape : outers) {
+						int tmpRes = IsInUtil.isLineInShape(kind, w.getPoints(), shape, elementBbox);
+						if (tmpRes != IsInUtil.OUT) {
+							result |= tmpRes;
+							if ((tmpRes & IsInUtil.IN) != 0) {
+								result = tmpRes;
+								break;
+							}
+						}
+					}
+					if ((result & IsInUtil.IN) != 0 && !holes.isEmpty()) {
+						// an outer ring matched
+						// check if any hole intersects with the way
+						for (List<Coord> hole : holes) {
+							int tmpRes = IsInUtil.isLineInShape(kind, w.getPoints(), hole, elementBbox);
+							if (tmpRes == IsInUtil.IN_ON_OUT)
+								return tmpRes;
+							if ((tmpRes & IsInUtil.IN) != 0) 
+								result = IsInUtil.OUT;
+							result |= tmpRes & IsInUtil.ON;
+							
+						}
+					}
+					
+				} else if (polygons.size() == 1) {
+					result = IsInUtil.isLineInShape(kind, w.getPoints(), (polygons.iterator().next()).getPoints(), elementBbox);
+				}
+			}
+		}
+		if (result == 0)
+			result = IsInUtil.OUT;
+		return result;
+	}
+
+	private static int dev_calcInsideness(FeatureKind kind, Element el, Set<Way> polygons) {
+		int result = 0;  
+		if (polygons == null || polygons.isEmpty()) { 
+			return IsInUtil.OUT;
+		}
+		Area tileBbox = null; // ??? Area.getBBox(el); maybe
+		IsInFunction anInst = new IsInFunction();
+		// [javac] /norbert/svn/branches/is-in/test/uk/me/parabola/util/IsInUtilTest.java:125: error: incompatible types: Set<Way> cannot be converted to Collection<Element>
+		//fix anInst.unitTestAugment(new ElementQuadTree(tileBbox, polygons));
+		anInst.setParams(Arrays.asList("landuse", "residential", "all"), kind);
+		String rslt = anInst.calcImpl(el);
+		/* TODO: %%%
+choose 3 methods depending on Kind whose combination will give us IN/ON/OUT 
+test the rslt string for "True" and set the bits in result as appropriate
+		*/
+		return result;
+	}
+ 
 	public static List<String> testWithVariants(FeatureKind kind, Element el, String name, Set<Way> polygons) {
 		List<String> errors = new ArrayList<>();
-		int res = IsInUtil.calcInsideness(kind, el, polygons);
+		int res = calcInsideness(kind, el, polygons);
 		
 		String expectedVal = el.getTag("expected");
 		if (expectedVal != null && !"?".equals(expectedVal)) {
@@ -54,7 +151,7 @@ public class IsInUtilTest {
 			if (el instanceof Way) {
 				Way w2 = (Way) el.copy();
 				Collections.reverse(w2.getPoints());
-				int res2 = IsInUtil.calcInsideness(kind, w2, polygons);
+				int res2 = calcInsideness(kind, w2, polygons);
 				if (expected != res2) {
 					errors.add(name + " failed reversed, expected: " + expected + " got " + res2);
 				}
@@ -64,7 +161,7 @@ public class IsInUtilTest {
 						points.remove(points.size() - 1);
 						Collections.rotate(points, 1);
 						points.add(points.get(0));
-						res2 = IsInUtil.calcInsideness(kind, w2, polygons);
+						res2 = calcInsideness(kind, w2, polygons);
 						if (expected != res2) {
 							errors.add(name + " failed rotated " + i + " , expected: " + expected + " got " + res2);
 						}
